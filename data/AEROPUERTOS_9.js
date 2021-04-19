36ed7efce6c371505c493c762dcc85f3996f262e
anfrodriguezca <andres.castiblanco@correounivalle.edu.co>
2021-04-18 23:26:55 -0500

    Initial commit

---
+++ data/AEROPUERTOS_9.js
@@ -0,0 +1,13 @@
+var json_AEROPUERTOS_9 = {
+"type": "FeatureCollection",
+"name": "AEROPUERTOS_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "NOMBRE": "AEROPUERTO SANTA ANA", "MUNICIPIO": "CARTAGO", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76147" }, "geometry": { "type": "Point", "coordinates": [ -75.95447491289994, 4.761620761717629 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "NOMBRE": "AEROPUERTO INTERNACIONAL ALFONSO BONILLA ARAGÓN", "MUNICIPIO": "PALMIRA", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76520" }, "geometry": { "type": "Point", "coordinates": [ -76.387920419539768, 3.536767342717849 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3", "NOMBRE": "AEROPUERTO GERARDO TOVAR LÓPEZ", "MUNICIPIO": "BUENAVENTURA", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76109" }, "geometry": { "type": "Point", "coordinates": [ -76.993429697497177, 3.820576768854341 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4", "NOMBRE": "AEROPUERTO HERIBERTO GIL MARTÍNEZ", "MUNICIPIO": "TULUÁ", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76834" }, "geometry": { "type": "Point", "coordinates": [ -76.224449343337838, 4.089268154103596 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "5", "NOMBRE": "AEROPUERTO JUANCHACO", "MUNICIPIO": "BUENAVENTURA", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76109" }, "geometry": { "type": "Point", "coordinates": [ -77.360828382247348, 3.93484569312307 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "6", "NOMBRE": "BASE AÉREA MARCO FIDEL SUÁREZ", "MUNICIPIO": "CALI", "DPTO_CCDGO": 76.0, "MPIO_CCDGO": "76001" }, "geometry": { "type": "Point", "coordinates": [ -76.497205818845558, 3.455968160636869 ] } }
+]
+}
